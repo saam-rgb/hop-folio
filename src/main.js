69f048fd2bfdb134d366b08cfff5a10f4e0c3c69
saam <saamsallvin@gmail.com>
2025-08-15 10:22:33 +0530

    vercel analytics added. reload 404 not found fix.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+if (import.meta.env.PROD) {
+  const script = document.createElement("script");
+  script.src = "/_vercel/insights/script.js";
+  script.defer = true;
+  document.head.appendChild(script);
+}
+
+createApp(App).use(router).mount("#app");
